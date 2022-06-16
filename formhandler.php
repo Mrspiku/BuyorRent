940a941f407bce4c67bd37b5b457908931fe4441
Priyanka <priyanka96325@gmail.com>
2022-06-16 20:13:29 +0530

    Initial commit

---
+++ formhandler.php
@@ -0,0 +1,26 @@
+<? php
+$name = $_POST['name'];
+$email_id = $_POST['email'];
+$city = $_POST['city'];
+$message = $_POST['message'];
+
+
+$email_from ='priyanka96325@gmail.com';
+$email_subject = 'New visitor';
+$email_body = "User Name: $name.\n".
+               "User Name: $email_id.\n".
+               "User Name: $city.\n".
+               "User Name: $message.\n";
+
+
+$to = 'priyanka96325@gmail.com';
+
+
+$headers ="From: $email_from \r\n";    //  \r is carriage return  
+
+$headers .="Reply-To: $email_id \r\n";
+
+
+mail($to, $email_subject,$email_body,$headers);
+header(Location: "contact.html");
+?>
